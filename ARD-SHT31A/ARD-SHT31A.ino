629553d81b6d891a515600acd76078b92b4dd627
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2017-12-02 21:57:48 +0700

    update  code convert celsius to fahrenheit

--- ARD-SHT31A/ARD-SHT31A.ino
+++ ARD-SHT31A/ARD-SHT31A.ino
@@ -21,6 +21,7 @@ const int PIN_T  = A0;
 const int PIN_RH = A1;  
 
 float t  = 0.0;
+float f  = 0.0;
 float rh = 0.0;
 
 void setup() {
@@ -32,12 +33,15 @@ void loop() {
   int tValue = analogRead(PIN_T);
   int rhValue = analogRead(PIN_RH);
 
-  t = -66.875+218.75*(tValue*0.0049)/VDD; 
-  rh = -12.5+125*(rhValue*0.0049)/VDD;
+  t = -66.875+218.75*(tValue*0.0049)/VDD;   // equation : -66.875+218.75*(Vt/Vdd)   | Vt = analogT*0.0049  
+  f = -88.375+393.75*(tValue*0.0049)/VDD;   // equation : -88.375+393.75*(Vt/Vdd)   | Vt = analogT*0.0049
+  rh = -12.5+125*(rhValue*0.0049)/VDD;      // equation : -12.5+125*(Vrh/Vdd)       | Vrh = analogRH*0.0049 
   
   Serial.print("T=");
   Serial.print(t);
-  Serial.print("C, RH=");
+  Serial.print("C, T=");
+  Serial.print(f);
+  Serial.print("F, RH=");
   Serial.print(rh);
   Serial.println("%");
   
